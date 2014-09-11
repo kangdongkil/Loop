510609abfe61c41120326c3c50d55cbedc521fe9
kangdongkil <kbk3418@naver.com>
2014-09-11 16:00:19 +0900

    a

--- sketch.js
+++ sketch.js
@@ -17,8 +17,15 @@ function setup() {
   ellipse(x*150, 100, 50, 50);
 }
 */
+//for(시작상태;반복하는 조건; 증가값)
 for(var x = 1; x<=7; x++)
 	{
 	ellipse(x*70,x*20 + 100, x * 30, x * 90);
 	}
+}
+
+function draw(){
+/*var size-Math.abs(mouseX -320);*/
+background(128,0,64);
+ellipse(320, 240, mouseX, mouseY);
 }
