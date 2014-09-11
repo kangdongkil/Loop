3a5f66351ba935374aa3146b07805c0198871459
kangdongkil <kbk3418@naver.com>
2014-09-11 15:12:07 +0900

    add

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
-function setup(){
-	createCanvas(680, 480);
-  	//0:black, 255:white
-  	//background(red,green.blue);
-  	background(255,0,0);
-  	stroke(255);
- 	  noFill();
- 	  stroke(0,255,0);
-	  line(10,10,90,10);
-	  stroke(2,80,147);
-	  rect(20,20,100,100);
-	  stroke(237,7,117);
-	  ellipse(320,240,100,50);
+function setup() {
+  createCanvas(640, 480);
+  background(128, 0, 64);
+  noFill();
 
+  stroke(255, 0, 0);
+
+  var x;
+
+  x = 100;
+  ellipse(x, 100, 50, 50);
+
+  x = 250;
+  ellipse(x, 100, 50, 50);
+
+  x = 300;
+  ellipse(x, 100, 50, 50);
 }
